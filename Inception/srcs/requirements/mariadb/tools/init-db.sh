0bdaf38b129f208950ba80bb425d5bcb1f12033f
helog234 <helenegandar@ik.me>
2024-10-15 17:30:16 +0200

    form corrections and comments

--- Inception/srcs/requirements/mariadb/tools/init-db.sh
+++ Inception/srcs/requirements/mariadb/tools/init-db.sh
@@ -8,69 +8,74 @@ DB_USER=$(printenv DB_USER)
 
 # Vérifier que les variables d'environnement sont définies
 if [[ -z "$DB_PASSWORD" || -z "$DB_ROOT_PASSWORD" || -z "$DB_NAME" || -z "$DB_USER" ]]; then
-    echo "Erreur : Les variables d'environnement ne sont pas correctement définies."
+    echo -e "${RED}DB:Erreur : Les variables d'environnement ne sont pas correctement définies.${NC}"
     exit 1
 fi
 
-# Codes de couleurs ANSI pour le logging
+# couleur <3
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # Réinitialiser la couleur
+NC='\033[0m'
 
-echo -e "${YELLOW}Démarrage du script d'initialisation de MariaDB...${NC}"
+echo -e "${YELLOW}DB:Démarrage du script d'initialisation de MariaDB...${NC}"
 
 # S'assurer que les répertoires ont les bonnes permissions
-echo "[DB config] Configuration des permissions pour MariaDB..."
+echo "DB:Configuration des permissions pour MariaDB..."
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
 # Initialiser la base de données si nécessaire
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "[DB config] Initialisation du répertoire de données de MariaDB..."
+    echo "DB:Initialisation du répertoire de données de MariaDB..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    echo "[DB config] Répertoire de données initialisé."
+    echo -e "${GREEN}DB: Répertoire de données initialisé.${NC}"
 else
-    echo "[DB config] Répertoire de données déjà existant, initialisation ignorée."
+    echo "DB: Répertoire de données déjà existant, initialisation ignorée."
 fi
 
-# Démarrer MariaDB en arrière-plan
-echo "[DB config] Démarrage de MariaDB..."
+# Démarrer mariadb en arrière-plan
+echo "DB: Démarrage de MariaDB..."
 /usr/bin/mysqld --user=mysql --console --skip-networking=0 --skip-bind-address &
 sleep 5
 
-# Attendre que MariaDB soit prêt
-echo "[DB config] Attente que MariaDB soit prêt..."
+#loop pour attendre que mariadbd soit pret
+echo "DB: Attente que MariaDB soit prêt..."
 until mysqladmin ping --silent; do
-    echo "MariaDB n'est pas encore prêt..."
+    echo -e "${YELLOW}MariaDB n'est pas encore prêt...${NC}"
     sleep 2
 done
 
-echo "[DB config] MariaDB est prêt."
+echo -e "${GREEN}DB: MariaDB est prêt.${NC}"
 
 # Remplacer les variables dans le fichier init.sql
+#obligée de le faire là car autrement impossible de
+#les récupérer automatiquement depuis le fichier sql
 sed -i "s/\${DB_ROOT_PASSWORD}/$DB_ROOT_PASSWORD/" /tmp/init.sql
 sed -i "s/\${DB_USER}/$DB_USER/" /tmp/init.sql
 sed -i "s/\${DB_PASSWORD}/$DB_PASSWORD/" /tmp/init.sql
 sed -i "s/\${DB_NAME}/$DB_NAME/" /tmp/init.sql
 
-# Exécuter le script SQL d'initialisation
-echo "[DB config] Exécution du script d'initialisation SQL..."
+#excution du script sql pour créer les user et droits d'accès
+echo "DB: Exécution du script d'initialisation SQL..."
 mysql -u root < /tmp/init.sql
 
 # Arrêter MariaDB
-echo "[DB config] Arrêt de MariaDB après l'initialisation..."
+echo "DB: Arrêt de MariaDB après l'initialisation..."
 mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
 
-echo "[DB config] Initialisation de MariaDB terminée."
+echo -e "${GREEN}DB: Initialisation de MariaDB terminée.${NC}"
 
-# Modifier la configuration pour autoriser les connexions distantes
-echo "[DB config] Configuration pour autoriser les connexions distantes..."
+#modifications des données de config
+#difficultés à connecter au bon port et domaine si je ne précisais pas 
+#le skip-networking à 0
+#0.0.0.0 permet d'écouter sur le réseau (inception dans notre cas)
+echo "DB: Configuration pour autoriser les connexions distantes..."
 sed -i "s|skip-networking|skip-networking = 0|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address = 0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-# Démarrer MariaDB en mode serveur
-echo "[DB config] Démarrage de MariaDB en mode serveur..."
+# démarrage de mariadb en mode serveur et définitif (avec les bonnes configurations)
+echo "DB: Démarrage de MariaDB en mode serveur..."
 exec /usr/bin/mysqld --user=mysql --console
--- Inception/srcs/requirements/wordpress/tools/wp-setup.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-setup.sh
@@ -1,53 +1,60 @@
 #!/bin/bash
 
-# Lire les secrets
+# lit les secrets, plus sur que ce soit % nécessaire mais ca pu faire
+#bugger parfois
 DB_PASSWORD=$(cat /run/secrets/db_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_super_password)
 WP_SECOND_USER_PASSWORD=$(cat /run/secrets/wp_random_password)
 
-# Codes de couleurs ANSI for logging
+# la vie est plus simple avec des couleurs
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # Reset color
+NC='\033[0m'
 
-# Vérifier que les secrets sont chargés correctement
+# checker secrets chargés
 if [[ -z "$DB_PASSWORD" || -z "$WP_ADMIN_PASSWORD" || -z "$WP_SECOND_USER_PASSWORD" ]]; then
-    echo "Error: Secrets are not properly loaded."
+    echo -e "${RED}WP: Error: Secrets are not properly loaded.${NC}"
     exit 1
 fi
 
-# Attendre que MariaDB soit disponible
+# check si mariadb est dispo
 TIMEOUT=60
 until mysqladmin ping -h mariadb -u"${DB_USER}" -p"${DB_PASSWORD}" --silent >/dev/null 2>&1 || [ $TIMEOUT -eq 0 ]; do
-    echo "Waiting for MariaDB to be available..."
+    echo -e "${YELLOW}WP: Waiting for MariaDB to be available...${NC}"
     sleep 2
     TIMEOUT=$((TIMEOUT-2))
 done
 
 if [ $TIMEOUT -eq 0 ]; then
-    echo "Error: MariaDB did not become available in time."
+    echo -e "${RED}WP: Error: MariaDB did not become available in time.${NC}"
     exit 1
 fi
 
-# Configurer WordPress (en modifiant wp-config.php via WP-CLI)
+# Configurer wp: connection a mariadb, users (grâce à notre fichier de config et cli)
+echo "Wordpress configuration"
 cd /var/www/wordpress
 if [ ! -f wp-config.php ]; then
-   wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="mariadb" --allow-root
+    wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="mariadb" --allow-root
+    echo -e "${GREEN}WP: connected to mariadb ${NC}"
 fi
 
-# Installer WordPress si ce n'est pas déjà fait
+# installer wp (si pas déjà) et ajouter le user admin dessus
+echo "Adding Admin user"
 if ! wp core is-installed --allow-root; then
     wp core install --url=${DOMAIN_NAME} --title="Inception" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
-fi
-echo -e "${YELLOW}IS THIS HERE ? ${NC}"
-# Ajouter un deuxième utilisateur
-if ! wp user get ${WP_SECOND_USER} --allow-root > /dev/null 2>&1; then
-    wp user create ${WP_SECOND_USER} ${WP_SECOND_USER_EMAIL} --role=editor --user_pass=${WP_SECOND_USER_PASSWORD} --allow-root
+    echo -e "${GREEN}WP: admin user added ${NC}"
 fi
 
-# Démarrer PHP-FPM en mode foreground
+#ajout un deuxième utilisateur
+if ! wp user get ${WP_SECOND_USER} --allow-root > /dev/null 2>&1; then
+    wp user create ${WP_SECOND_USER} ${WP_SECOND_USER_EMAIL} --role=editor --user_pass=${WP_SECOND_USER_PASSWORD} --allow-root
+    echo -e "${GREEN}WP: second user added ${NC}"
+fi
+
+# Démarrer PHP-FPM en mode foreground (avant plan)
+#option -F pas en arrière plan, pour que Docker garde le conteneur exec tant que php est actif.
 exec php-fpm82 -F
 
 
